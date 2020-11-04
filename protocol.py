9582ad5e012c2d24d54916ca728d12fcec190491
Antoine Rozo <antoine.rozo@gmail.com>
2020-11-04 18:32:38 +0100

    Staging protocol packets

--- protocol.py
+++ protocol.py
@@ -33,6 +33,10 @@ _commands = {
     'rotate_angle': (1, 12, 'i'),  # angle in decidegrees
     'drive_arc': (1, 27, 'ii'),  # angle in decidegrees, radius in mm
 
+    'reset_position': (1, 15, ''),
+    'get_position': (1, 16, ''),
+    'nav_to_position': (1, 17, '3h'),  # X in mm, Y in mm, orientation in decidegrees (-1 to let it choice)
+
     'set_marker_eraser': (2, 0, 'B'),  # position (0=up|1=marker down|2=eraser down)
     'get_color_data': (4, 1, '3B'),  # sensor (0 to 3, from left to right), lightning (0=off|1=red|2=green|3=blue|4=all), format (0=12bit|1=mv)
 
@@ -41,8 +45,13 @@ _commands = {
     'play_note': (5, 0, 'IH'),  # freq in Hz, duration in ms
     'stop_note': (5, 1, ''),
     'say_phrase': (5, 4, '16s'),  # phrase (bytes, utf8 encoded)
+    'play_sweep': (5, 5, '2IH6B'),  # start freq in mHz, end freq in mHz, duration in ms, attack in ms, release in ms, volume, modulation type, modulation rate, append
+
+    'get_light_values': (13, 1, ''),
 
     'get_battery_level': (14, 1, ''),
+
+    'get_accelerometer': (16, 1, ''),
 }
 
 _events = {
@@ -57,16 +66,21 @@ _events = {
     (1, 8): ('drive_distance_finished', ''),
     (1, 12): ('rotate_angle_finished', ''),
     (1, 27): ('drive_arc_finished', ''),
+    (1, 16): ('get_position', 'I3h'),  # X in mm, Y in mm, orientation in decidegrees
+    (1, 17): ('nav_to_position_finished', '3h'),  # X in mm, Y in mm, orientation in decidegrees
     (1, 29): ('motor_stall', 'I2B'),  # timestamp, motor (0=left|1=right|2=marker), cause
     (2, 0): ('marker_eraser_finished', 'B'),  # position
     (4, 1): ('color_response', '8H'),  # 8 16bit colors
     (4, 2): ('color_event', '16s'),  #
     (5, 0): ('play_note_finished', ''),
     (5, 4): ('say_phrase_finished', ''),
+    (5, 5): ('play_sweep_finished', ''),
     (12, 0): ('bumper_event', 'IB'),  # timestamp, state(00=no|40=right|80=left|C0=both)
     (13, 0): ('light_event', 'IB2H'),  # timestamp, state, left, right
+    (13, 1): ('light_values', 'I2H'),  # timestamp, left, right
     (14, 0): ('battery_event', 'IHB'),  # timestamp, voltage, percent
     (14, 1): ('battery_response', 'IHB'),  # timestamp, voltage, percent
+    (16, 1): ('accelerometer', 'I3h'),  # timestamp, X, Y, Z
     (17, 0): ('touch_event', 'IB'),  # timestamp, state
     (20, 0): ('cliff_event', 'IB2H'),  # timestamp, cliff, sensor, threshold
 }
