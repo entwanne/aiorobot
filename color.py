3792bd46bb2b053dad123dfe68e3cf65ee87b5f0
Antoine Rozo <antoine.rozo@gmail.com>
2021-02-04 18:26:35 +0100

    colors tk

--- color.py
+++ color.py
@@ -1,4 +1,5 @@
-from aiorobot import run
+#from aiorobot import run
+from aiorobot.examples.thread import run_thread, queue as q
 from aiorobot.types import *
 
 async def start(robot):
@@ -19,4 +20,38 @@ async def start(robot):
 
     await robot.disconnect()
 
-run(started=start)
+async def touch(robot):
+    red = max(await robot.color.get(ColorSensor.LEFT, ColorLightning.RED, ColorFormat.ADC))
+    green = max(await robot.color.get(ColorSensor.LEFT, ColorLightning.GREEN, ColorFormat.ADC))
+    blue = max(await robot.color.get(ColorSensor.LEFT, ColorLightning.BLUE, ColorFormat.ADC))
+    print(red, green, blue)
+
+    red = min(int(255 * red / 500), 255)
+    green = min(int(255 * green / 200), 255)
+    blue = min(int(255 * blue / 800), 255)
+
+    print(red, green, blue)
+    return red, green, blue
+
+async def start_tk(robot):
+    await q.get()
+    await robot.disconnect()
+
+async def touch_tk(robot):
+    r, g, b = await touch(robot)
+    color = f'#{r:02x}{g:02x}{b:02x}'
+    print(color)
+    canvas.configure(bg=color)
+
+#run(started=start)
+#run(stopped=touch)
+run_thread(started=start_tk, stopped=touch_tk)
+
+import tkinter as tk
+root = tk.Tk()
+canvas = tk.Canvas(root)
+canvas.pack()
+try:
+    root.mainloop()
+finally:
+    q.put_nowait(None)
